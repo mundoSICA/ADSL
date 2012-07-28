4b40d4640e93aa60b4ed160b79db0e33eed5e0c3
Fitorec <chanerec@gmail.com>
2012-07-28 12:21:02 -0500

    Creando un shell para el seteo de contraseñas.
    La mejor solución es implementar un mecanismo para el seteo de
    contraseñas pero por lo pronto esto me resuelve una incurrencia
    que tendo de momento.

---
+++ app/Console/Command/UserShell.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * Shell util para cambiar el password a usuarios
+ */
+class UserShell extends AppShell {
+		public $uses = array('User');
+		public function main() {
+				$this->User->order = 'User.id';
+				$users = $this->User->find('list');
+				foreach ($users as $user_id => $user_nickname) :
+					$prev = str_repeat(' ', 3 - strlen($user_id)) . $user_id;
+					$this->out( $prev . ".- <warning>" . $user_nickname . '</warning>');
+				endforeach;
+				$id = $this->in('Seleccione un numero');
+				if (!isset($users[$id])) {
+					return 0;
+				}
+				$this->out('Usuario seleccionado <warning>'.$users[$id].'</warning>');
+				$this->User->recursive = -1;
+				$this->User->id = $id;
+				$user = $this->User->read();
+				$user['User']['password'] = $this->password();
+				if( $this->User->save($user) ){
+						$this->out('La contraseña fue cambiada con exito');
+				}else{
+						$this->out('Error al intentar cambiar al contraseña');
+				}
+		}
+		
+		/**
+		 * Mi de una nueva contraseña y regresa el hash del mismo
+		 *
+		 * @return El hash correspondiente al password
+		 * @access publico
+		 */
+		function password() {
+			$pass_plain = $this->in('El nuevo password: ');
+			App::import('Component', 'Auth');
+			$auth =& new AuthComponent(null);
+			var_dump($auth->password($pass_plain));
+			return $auth->password($pass_plain);
+		}
+}
